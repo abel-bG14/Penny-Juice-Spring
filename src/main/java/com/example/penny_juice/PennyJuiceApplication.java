bab2a2a19109b9905fd7cedc39bf14fcf820c2f8
Sebastian <ebastborj5@gmail.com>
2025-10-23 15:29:18 -0500

    Initial commit - proyecto Spring Boot Penny Juice

---
+++ src/main/java/com/example/penny_juice/PennyJuiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.penny_juice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PennyJuiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PennyJuiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/penny_juice/PennyJuiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.penny_juice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PennyJuiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
