86c2fab757748ee43ed5fc0611c9927dbbf11bcf
Josue049 <josue09guti@gmail.com>
2025-11-16 15:11:00 -0500

    pasarela de pagos

--- src/main/java/com/example/penny_juice/controller/HomeController.java
+++ src/main/java/com/example/penny_juice/controller/HomeController.java
@@ -1,10 +1,24 @@
 package com.example.penny_juice.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.example.penny_juice.model.Producto;
+import com.example.penny_juice.service.ProductoService;
+
 
 @Controller
 public class HomeController {
+
+    private final ProductoService productoService;
+
+    // Inyección por constructor
+    public HomeController(ProductoService productoService) {
+        this.productoService = productoService;
+    }
+
     @GetMapping({ "/", "/index" })
     public String index() {
         return "index";
@@ -14,6 +28,28 @@ public class HomeController {
     public String login() {
         return "login";
     }
+
+    @GetMapping("/galeria")
+    public String galeria(Model model) {
+        model.addAttribute("listaProductos", productoService.listar());
+        return "galeria";
+    }
+
+    @GetMapping("/carrito")
+    public String carrito() {
+        return "carrito";
+    }
+
+    @GetMapping("/galeria/{id}")
+    public String detalleProducto(@PathVariable Long id, Model model) {
+        Producto producto = productoService.obtenerPorId(id); // devuelve Producto o null
+        if (producto == null) {
+            return "redirect:/galeria"; // o una página de error personalizada
+        }
+        model.addAttribute("producto", producto);
+        return "detalle-producto"; // nombre del HTML Thymeleaf
+    }
+
 
     @GetMapping("/register")
     public String register() {
